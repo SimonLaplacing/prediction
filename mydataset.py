393c8fa914782bcff3fa49d7a75ca751b4dde5fe
SimonLaplacing <540379303@qq.com>
2021-05-13 13:24:30 +0800

    updated

--- mydataset.py
+++ mydataset.py
@@ -1,17 +1,22 @@
+from l5kit.data import ChunkedDataset
+from l5kit.dataset import AgentDataset #, EgoDataset
+from l5kit.rasterization import build_rasterizer
+from torch.utils.data import get_worker_info
+from typing import Optional
+import numpy as np
+
 class MyTrainDataset:
-    def __init__(self, cfg, dm):
+    def __init__(self, cfg, dm, agents_mask: Optional[np.ndarray] = None):
         self.cfg = cfg
         self.dm = dm
         self.has_init = False
+        self.agents_mask = agents_mask
     def initialize(self, worker_id):
         print('initialize called with worker_id', worker_id)
-        from l5kit.data import ChunkedDataset
-        from l5kit.dataset import AgentDataset #, EgoDataset
-        from l5kit.rasterization import build_rasterizer
         rasterizer = build_rasterizer(self.cfg, self.dm)
         train_cfg = self.cfg["train_data_loader"]
         train_zarr = ChunkedDataset(self.dm.require(train_cfg["key"])).open(cached=False)  # try to turn off cache
-        self.dataset = AgentDataset(self.cfg, train_zarr, rasterizer)
+        self.dataset = AgentDataset(self.cfg, train_zarr, rasterizer, self.agents_mask)
         self.has_init = True
     def reset(self):
         self.dataset = None
@@ -22,7 +27,6 @@ class MyTrainDataset:
     def __getitem__(self, index):
         return self.dataset[index]    
 
-from torch.utils.data import get_worker_info
 def my_dataset_worker_init_func(worker_id):
     worker_info = get_worker_info()
     dataset = worker_info.dataset
